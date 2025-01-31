6b26614e9d3fa6ca63e477166c63ef46bca26fb6
Hayden Eubanks <haydenubanx@gmail.com>
2025-01-31 18:26:39 +0000

    Initial Commit

---
+++ Demo Project/Demo_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_ProjectApp.swift
+//  Demo Project
+//
+//  Created by Hayden Work on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Demo_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo ProjectTests/Demo_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_ProjectTests.swift
+//  Demo ProjectTests
+//
+//  Created by Hayden Work on 1/31/25.
+//
+
+import Testing
+@testable import Demo_Project
+
+struct Demo_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
